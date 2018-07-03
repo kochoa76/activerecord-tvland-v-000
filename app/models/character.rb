c97992564bfaf6a161faecf7d5a6a65d005bbd4b
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-03 20:38:03 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,9 +1,9 @@
 class Character < ActiveRecord::Base
   belongs_to :show
-  belongs_to :actor 
+  belongs_to :actor
 
-  def say_that_thing_you_say 
+  def say_that_thing_you_say
     "#{self.name} always say: self.catchphrase"
-  end 
-  
+  end
+
 end
